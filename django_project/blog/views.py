6e30c061f89d981121efa14e478519afc767516a
ranajaydas <ranajay@outlook.com>
2019-10-11 14:42:47 +0200

    Added dummy posts, css and superuser to blog.

---
+++ django_project/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+
+posts = [
+    {
+        'author': 'Ranajay',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 11, 2019'
+    },
+    {
+        'author': 'Pootpoot',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 10, 2019'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
