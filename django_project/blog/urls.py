4147ae58cfad835cbfa2c5088921becf2476ba42
ranajaydas <ranajay@outlook.com>
2019-10-14 10:48:22 +0200

    Create, Update and Delete posts

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from .views import (PostListView, PostDetailView, PostCreateView,
+                    PostUpdateView, PostDeleteView)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),        # <pk> is <primary key>
+    path('post/new/', PostCreateView.as_view(), name='post-create'),             # Linked to post_form.html
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),  # Linked to post_form.html
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),  # Linked to post_confirm_delete.html
     path('about/', views.about, name='blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,13 +1,63 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import (ListView, DetailView, CreateView,
+                                  UpdateView, DeleteView)
 from .models import Post
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+class PostListView(ListView):
+    """Class based view for Home page."""
+    model = Post
+    template_name = 'blog/home.html'        # Default = <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'           # Variable inside blog/home.html (default = object_list)
+    ordering = ['-date_posted']             # Orders the posts by newest to oldest
+
+
+class PostDetailView(DetailView):
+    """Class based view for individual posts."""
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """Class based view for users to create new posts."""
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        """Required to override default form_valid method."""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    """Class based view for users to update their posts."""
+    model = Post
+    fields = ['title', 'content']
+    success_message = 'Post updated!'
+
+    def form_valid(self, form):
+        """Required to override default form_valid method."""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        """To ensure that only the author can change their own post."""
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Class based view for users to delete their posts."""
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        """To ensure that only the author can change their own post."""
+        post = self.get_object()
+        return self.request.user == post.author
 
 
 def about(request):
+    """Function based view for About page."""
     return render(request, 'blog/about.html', {'title': 'About'})
