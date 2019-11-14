35a2ac88695d97779bfb664dd8dcf239259cedc9
ranajaydas <ranajay@outlook.com>
2019-11-14 08:50:42 +0100

    Added comment for best practises when importing User model

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  # This is not the best way to do this
 from PIL import Image
 
 
