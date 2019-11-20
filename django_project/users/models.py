8cd8caa2f3670d943ef3ac183edba65c108bbfe7
ranajaydas <ranajay@outlook.com>
2019-11-20 16:28:19 +0100

    Removed comment

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User  # This is not the best way to do this
+from django.contrib.auth.models import User
 from PIL import Image
 
 
